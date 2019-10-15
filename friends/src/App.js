b7f6918db8ed0fce63e187e6885a369cf1b8d2b0
CScori <amilanotte1366@gmail.com>
2019-10-15 15:03:03 -0500

    adds app set up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Friends
-    </div>
+      <Router>
+        <div className="App">
+          <h1>Friends</h1>
+          <nav>
+          <ul>
+            <li>
+              <Link>Login</Link>
+            </li>
+            <li>
+              <Link>Friends</Link>
+            </li>
+          </ul>
+          </nav>
+          <Switch>
+            <Route path="/login"/>
+            <Route path="friends"/>
+          </Switch>
+        </div>
+      </Router>
+    
   );
 }
 
